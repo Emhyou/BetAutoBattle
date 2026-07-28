5641c0f1e6152b7b830cd07f3349178de1cc0fe0
Emhyou <0120.mizugame@gmail.com>
2026-07-28 10:35:08 +0900

    プロジェクトファイルを追加。

---
+++ Battle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetAutoBattle
+{
+    internal class Battle
+    {
+        public static List<Monster> BuildTeam(List<MonsterData> pool, int budget, int maxMembers)
+        {
+            List<Monster> team = new List<Monster>();
+            int usedCost = 0;
+            Random localRand = new Random();
+            int failCount = 0;
+
+            while (usedCost < budget && failCount < 10 && team.Count < maxMembers)
+            {
+                var candidate = pool[localRand.Next(pool.Count)];
+
+                if (usedCost + candidate.Cost > budget)
+                {
+                    failCount++;
+                    continue;
+                }
+
+                team.Add(new Monster(candidate.Name, candidate.Hp, candidate.Attack, candidate.Speed, candidate.Cost));
+                usedCost += candidate.Cost;
+                failCount = 0;
+            }
+
+            return team;
+        }
+        public static Monster DetermineFirst(Monster a, Monster b)
+        {
+            return a.speed >= b.speed ? a : b;
+        }
+
+        public static void Attack(Monster attacker, Monster target)
+        {
+            target.hp -= attacker.attack;
+            Console.WriteLine($"{attacker.name}が{target.name}に{attacker.attack}のダメージ！残りHPは{target.hp}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace BetAutoBattle
+{
+    public class MonsterData
+    {
+        public string Name { get; set; }
+        public int Hp { get; set; }
+        public int Attack { get; set; }
+        public int Speed { get; set; }
+        public int Cost { get; set; }
+        public int BlockRate { get; set; }
+        public int DodgeRate { get; set; }
+    }
+
+    [XmlRoot("Monsters")]
+    public class MonsterList
+    {
+        [XmlElement("Monster")]
+        public List<MonsterData> Monsters { get; set; }
+    }
+
+    internal class Monster
+    {
+        internal static Random rand = new Random();
+        public string name { get; set; }
+        private int _hp;
+        public int hp
+        {
+            get { return _hp; }
+            set { _hp = value < 0 ? 0 : value; }
+        }
+        public int attack { get; set; }
+        public int speed { get; set; }
+
+        public int cost { get; set; }
+
+        public Monster(string name, int hp, int attack, int speed,int cost)
+        {
+            this.name = name;
+            this.hp = hp;
+            this.attack = attack;
+            this.speed = speed;
+            this.cost = cost;
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            string path = "Monsters.xml";
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(MonsterList));
+            using (System.IO.FileStream stream = new System.IO.FileStream(path, System.IO.FileMode.Open))
+            {
+                MonsterList monsterList = (MonsterList)xmlSerializer.Deserialize(stream);
+
+                List<Monster> teamA = Battle.BuildTeam(monsterList.Monsters, 30, 4);
+                List<Monster> teamB = Battle.BuildTeam(monsterList.Monsters, 40, 3);
+
+                Console.WriteLine("チームA:");
+                foreach (var m in teamA)
+                {
+                    Console.WriteLine($"{m.name} (Cost:{m.cost})");
+                }
+
+                Console.WriteLine("チームB:");
+                foreach (var m in teamB)
+                {
+                    Console.WriteLine($"{m.name} (Cost:{m.cost})");
+                }
+            }
+        }
+    }
+}
